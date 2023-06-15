8f586ae292dec507f77706357efaa87b39894c21
Son Hai Pham <psonhai.it@gmail.com>
2023-06-15 11:53:13 -0700

    week 3 carmodel, carmake model

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarModel, CarMake
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+  model = CarModel
 
 # CarModelAdmin class
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+  pass
 
 # CarMakeAdmin class with CarModelInline
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+  inlines = [CarModelInline]
 
 # Register models here
